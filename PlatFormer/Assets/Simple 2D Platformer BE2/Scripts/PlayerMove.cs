130f8edf1c75c829889177932342767a37cb15d8
ChopangE <chosw05222@gmail.com>
2024-03-07 15:40:31 +0900

    03_07_3_40

--- PlatFormer/Assets/Simple 2D Platformer BE2/Scripts/PlayerMove.cs
+++ PlatFormer/Assets/Simple 2D Platformer BE2/Scripts/PlayerMove.cs
@@ -25,6 +25,8 @@ public class PlayerMove : MonoBehaviour
             rb.velocity = Vector2.zero;
             rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             jumpCount++;
+            anim.SetBool("Jump", true);
+
         }
         else if (Input.GetButtonUp("Jump") && rb.velocity.y > 0) {
             rb.velocity = rb.velocity * 0.5f;
@@ -58,18 +60,37 @@ public class PlayerMove : MonoBehaviour
         else if(rb.velocity.x < -maxSpeed) {
             rb.velocity = new Vector2 (-maxSpeed, rb.velocity.y);
         }
-       
+        
+        //if (rb.velocity.y < 0) {
+        //    Debug.DrawRay(rb.position, Vector2.down, Color.red);
+        //    RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.down, 1, LayerMask.GetMask("Ground"));
+        //    if(hit.collider != null) {
+        //        if (hit.distance < 0.5f) {
+        //            anim.SetBool("Jump", false);
+        //            jumpCount = 0;
+        //        }
+                
+        //    }
+        //}
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log(collision.gameObject.tag);
-        
-        anim.SetBool("Jump", false);
-        jumpCount = 0;
-        rb.velocity = Vector2.zero;
-        
+        if (collision.contacts[0].normal.y > 0.6f) {
+            anim.SetBool("Jump", false);
+            if(jumpCount > 0) rb.velocity = Vector2.zero;
+
+            jumpCount = 0;
+            Debug.Log("Ground");
+
+        }
+        else {
+            Debug.Log("Ceil");
+
+        }
+
     }
     void OnCollisionExit2D(Collision2D collision) {
         anim.SetBool("Jump", true);
+
     }
 }
